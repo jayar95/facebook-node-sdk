c8cb1ce2cefb4898b1c6fa69aadb43522a5f0a4d
Jayar95 <lopez.jr1995@gmail.com>
2017-05-31 21:41:37 -0400

    build

--- test/_supports/notError.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var expect = require('chai').expect;
-
-module.exports = function(result) {
-	expect(result.error && result.error.Error).to.not.exist;
-	expect(result.error).to.not.be.instanceof(Error);
-};
--- test/lint.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var lint = require('mocha-eslint');
-
-if ( !process.env.CI_TEST || process.env.CI_TEST !== 'no-lint' ) {
-	lint(['.']);
-}
